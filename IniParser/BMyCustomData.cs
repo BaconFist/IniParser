ebe3559b33a3153425406bf5305e90687b7add6f
Thomas Klose <thomas@bratler.net>
2017-01-08 01:19:08 +0100

    1st Version & Tests

---
+++ IniParser/BMyCustomData.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IniParser
+{
+    public class BMyCustomData
+    {
+        private Dictionary<string, Dictionary<string, string>> Data;
+
+        public BMyCustomData(string data)
+        {
+            Data = (new Serializer()).deserialize(data.Split(new Char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries));
+        }
+
+        public string getSerialized()
+        {
+            return (new Serializer()).serialize(Data);
+        }
+
+        public int CountSections()
+        {
+            return Data.Count;
+        }
+
+        public int CountValues(string section)
+        {
+            if (hasSection(section))
+            {
+                return Data[section].Count;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public bool hasSection(string section)
+        {
+            return Data.ContainsKey(section);
+        }
+
+        public bool hasValue(string section, string key)
+        {
+            return hasSection(section) && Data[section].ContainsKey(key);
+        }
+
+        public string getValue(string section, string key)
+        {
+            return hasValue(section, key) ? Data[section][key] : null;
+        }
+
+        public bool addValue(string section, string key, string value)
+        {
+            if (!addSection(section))
+            {
+                return false;
+            }
+            if (hasValue(section, key))
+            {
+                Data[section][key] = value;
+            }
+            else
+            {
+                Data[section].Add(key, value);
+            }
+
+            return hasValue(section, key);
+        }
+
+        public bool addSection(string section)
+        {
+            if (-1 == section.IndexOfAny(new Char[] { '[', ']' }))
+            {
+                if (!hasSection(section))
+                {
+                    Data.Add(section, new Dictionary<string, string>());
+                }
+            }
+
+            return hasSection(section);
+        }
+
+        public Dictionary<string, string> getSection(string section)
+        {
+            return hasSection(section) ? Data[section] : null;
+        }
+
+        private class Serializer
+        {
+            public string serialize(Dictionary<string, Dictionary<string, string>> Data)
+            {
+                StringBuilder SerializedData = new StringBuilder();
+                foreach (KeyValuePair<string, Dictionary<string, string>> Section in Data)
+                {
+                    if (Section.Value.Count > 0)
+                    {
+                        SerializedData.AppendLine(string.Format(@"[{0}]", Section.Key));
+                        foreach (KeyValuePair<string, string> keyValueBuff in Section.Value)
+                        {
+                            if (keyValueBuff.Value.EndsWith(" "))
+                            {
+                                SerializedData.AppendLine(string.Format(@"{0}=""{1}""", keyValueBuff.Key, keyValueBuff.Value));
+                            }
+                            else
+                            {
+                                SerializedData.AppendLine(string.Format(@"{0}={1}", keyValueBuff.Key, keyValueBuff.Value.Trim()));
+                            }
+                        }
+                    }
+                }
+                return SerializedData.ToString();
+            }
+
+            public Dictionary<string, Dictionary<string, string>> deserialize(string[] sourceRaw)
+            {
+                Dictionary<string, Dictionary<string, string>> DeserialzedData = new Dictionary<string, Dictionary<string, string>>();
+                string currentSection = null;
+                foreach (string line in sourceRaw)
+                {
+                    if (isComment(line))
+                    {
+                        continue;
+                    }
+                    else if (isSection(line))
+                    {
+                        currentSection = getSectionName(line);
+                    }
+                    else if (currentSection != null && isKeyValuePair(line))
+                    {
+                        string keyBuffer = "";
+                        string valueBuffer = "";
+                        if (TryGetKeyValuePair(line, out keyBuffer, out valueBuffer))
+                        {
+                            if (!DeserialzedData.ContainsKey(currentSection))
+                            {
+                                DeserialzedData.Add(currentSection, new Dictionary<string, string>());
+                            }
+                            if (DeserialzedData[currentSection].ContainsKey(keyBuffer))
+                            {
+                                DeserialzedData[currentSection][keyBuffer] = DeserialzedData[currentSection][keyBuffer] + "\n" + valueBuffer;
+                            }
+                            else
+                            {
+                                DeserialzedData[currentSection].Add(keyBuffer, valueBuffer);
+                            }
+                        }
+                    }
+                }
+
+                return DeserialzedData;
+            }
+
+            private bool TryGetKeyValuePair(string source, out string key, out string value)
+            {
+                KeyValuePair<string, string> result = new KeyValuePair<string, string>(null, null);
+                string slug = source.Trim();
+                string[] data = slug.Split(new Char[] { '=' }, 2);
+                key = null;
+                value = null;
+                if (data.Length == 2)
+                {
+                    key = data[0];
+                    value = data[1].Trim();
+                    if (value.StartsWith("\""))
+                    {
+                        int lastIndexQuote = value.LastIndexOf('"');
+                        if (lastIndexQuote != -1)
+                        {
+                            value = value.Substring(1, lastIndexQuote-1);
+                        }
+                    }
+                    return true;
+                }
+                return false;
+            }
+
+            private string getSectionName(string value)
+            {
+                return value.Trim().TrimStart(new Char[] { '[' }).TrimEnd(new Char[] { ']' });
+            }
+
+            private bool isKeyValuePair(string value)
+            {
+                return (new System.Text.RegularExpressions.Regex(@"^[^=]+=[\S\s]*$")).IsMatch(value);
+            }
+
+            private bool isComment(string value)
+            {
+                return (new System.Text.RegularExpressions.Regex(@"^\s*;")).IsMatch(value);
+            }
+
+            private bool isSection(string value)
+            {
+                return (new System.Text.RegularExpressions.Regex(@"^\s*\[[^\]]+\]\s*$")).IsMatch(value);
+            }
+        }
+    }
+}
---
+++ IniParserTests/BMyCustomDataTests.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IniParser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IniParser.Tests
+{
+    [TestClass()]
+    public class BMyCustomDataTests
+    {
+        private string exampleINI = @"; last modified 1 April 2001 by John Doe
+[owner]
+name=John Doe
+organization=Acme Widgets Inc.
+
+[database]
+; use IP address in case network name resolution is not working
+server=192.0.2.62     
+port=143
+file=""payroll.dat""";
+
+        [TestMethod()]
+        public void BMyCustomDataTest()
+        {
+            BMyCustomData Mock = new BMyCustomData("");
+
+            Assert.IsInstanceOfType(Mock, typeof(BMyCustomData));
+        }
+
+        [TestMethod()]
+        public void getSerializedTest()
+        {
+            BMyCustomData Mock = new BMyCustomData("");
+            Mock.addValue("Section1", "Key1", "Value1");
+            Mock.addValue("Section1", "Key2", "Value2");
+            string testData = Mock.getSerialized();
+            Assert.AreEqual("[Section1]\r\nKey1=Value1\r\nKey2=Value2\r\n", testData);
+        }
+
+        [TestMethod()]
+        public void CountSectionsTest()
+        {
+            BMyCustomData Mock = new BMyCustomData(exampleINI);
+            Assert.AreEqual(2, Mock.CountSections());
+            Mock.addSection("Derp");
+            Assert.AreEqual(3, Mock.CountSections());
+        }
+
+        [TestMethod()]
+        public void CountValuesTest()
+        {
+            BMyCustomData Mock = new BMyCustomData(exampleINI);
+            Assert.AreEqual(2, Mock.CountValues("owner"));
+            Assert.AreEqual(3, Mock.CountValues("database"));
+            Assert.AreEqual(0, Mock.CountValues("[owner]"));
+        }
+
+        [TestMethod()]
+        public void hasSectionTest()
+        {
+            BMyCustomData Mock = new BMyCustomData(exampleINI);
+            Assert.IsTrue(Mock.hasSection("owner"));
+            Assert.IsTrue(Mock.hasSection("database"));
+            Assert.IsFalse(Mock.hasSection("[owner]"));
+            Assert.IsFalse(Mock.hasSection("Database"));
+            Assert.IsFalse(Mock.hasSection("foo"));
+            Assert.IsFalse(Mock.hasSection("bar"));
+        }
+
+        [TestMethod()]
+        public void getValueTest()
+        {
+            BMyCustomData Mock = new BMyCustomData(exampleINI);
+            Assert.IsNotNull(Mock.getValue("owner", "name"));
+            Assert.IsNotNull(Mock.getValue("owner", "organization"));
+            Assert.IsNotNull(Mock.getValue("database", "server"));
+            Assert.IsNotNull(Mock.getValue("database", "port"));
+            Assert.IsNotNull(Mock.getValue("database", "file"));
+            Assert.AreEqual("John Doe", Mock.getValue("owner", "name"));
+            Assert.AreEqual("Acme Widgets Inc.", Mock.getValue("owner", "organization"));
+            Assert.AreEqual("192.0.2.62", Mock.getValue("database", "server"));
+            Assert.AreEqual("143", Mock.getValue("database", "port"));
+            Assert.AreEqual("payroll.dat", Mock.getValue("database", "file"));
+            Assert.IsNull(Mock.getValue("Foo", "Bar"));
+            Assert.IsNull(Mock.getValue("database", "name"));
+            Assert.IsNull(Mock.getValue("[owner]", "name"));
+            Assert.IsNull(Mock.getValue("[foo]", "port"));
+            Assert.IsNull(Mock.getValue("database", " file"));
+        }
+
+        [TestMethod()]
+        public void addValueTest()
+        {
+            BMyCustomData Mock = new BMyCustomData("[Section1]");
+            Assert.IsTrue(Mock.addValue("Section1", "foo", "bar"));
+            Assert.IsTrue(Mock.addValue("Section2", "foo", "bar"));
+            Assert.IsFalse(Mock.addValue("[Section3]", "foo", "bar"));
+        }
+
+        [TestMethod()]
+        public void addSectionTest()
+        {
+            BMyCustomData Mock = new BMyCustomData("");
+            Assert.IsTrue(Mock.addSection("Derp"));
+            Assert.IsFalse(Mock.addSection("[dulli]"));
+        }
+
+        [TestMethod()]
+        public void getSectionTest()
+        {
+            BMyCustomData Mock = new BMyCustomData(exampleINI);
+            Assert.IsNotNull(Mock.getSection("owner"));
+            Assert.IsNotNull(Mock.getSection("database"));
+            Assert.IsNull(Mock.getSection("[owner]"));
+            Assert.IsNull(Mock.getSection("[database]"));
+            Assert.IsNull(Mock.getSection("dulli"));
+        }
+
+        [TestMethod()]
+        public void hasValueTest()
+        {
+            BMyCustomData Mock = new BMyCustomData(exampleINI);
+            Assert.IsTrue(Mock.hasValue("owner", "name"));
+            Assert.IsTrue(Mock.hasValue("owner", "organization"));
+            Assert.IsTrue(Mock.hasValue("database", "server"));
+            Assert.IsTrue(Mock.hasValue("database", "port"));
+            Assert.IsTrue(Mock.hasValue("database", "file"));
+            Assert.IsFalse(Mock.hasValue("foo", "bar"));
+            Assert.IsFalse(Mock.hasValue("Database", "file"));
+            Assert.IsFalse(Mock.hasValue("owner ", "name"));
+        }
+    }
+}
