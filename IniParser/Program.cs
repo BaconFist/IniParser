cc9addb30f9b4dcef9c55026f25441e7d987cf46
Thomas Klose <thomas@bratler.net>
2017-01-18 19:13:48 +0100

    load and save ini for comparsion
    like with meld

--- IniParser/Program.cs
+++ IniParser/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace IniParser
 {
@@ -10,8 +11,20 @@ namespace IniParser
     {
         static void Main(string[] args)
         {
-        }
-
-        
+            if (args.Length > 0 && File.Exists(args[0]))
+            {
+                string input = File.ReadAllText(args[0]);
+                Console.WriteLine(string.Format("read {0} chars from {1}", input.Length, args[0]));
+                BMyIni ini = new BMyIni(input, "test");
+                string output = ini.getSerialized();
+                string savefile = string.Format(@"{0}.{1}.txt", args[0], Guid.NewGuid());
+                File.WriteAllText(savefile, output);
+                Console.WriteLine("saved {0} chars to {1}", output.Length, savefile);
+            } else
+            {
+                Console.WriteLine("args < 2");
+            }
+            Console.ReadLine();
+        }        
     }
 }
